885fe78c644c0a184784fad5d52828a73bc5629b
oliverunger <oliver@oliverunger.de>
2022-12-02 21:40:03 +0100

    Replaced increment and decrement implementations

--- src/main/ch.oliverunger/logic/circuits/circuits.ts
+++ src/main/ch.oliverunger/logic/circuits/circuits.ts
@@ -1,32 +1,22 @@
-/**
- * Quantum Random Number Generator (QRNG)
- */
 import {QUBIT_STATE_ZERO} from "../../model/qubit";
 import {had} from "../gates/single-qubit-gates";
 import {bit} from "../math/truth-table";
 import {QubitRegister} from "../../model/qubit-register";
-import {mct} from "../gates/multi-qubit-gates";
-import {range, rotateArray} from "../../util";
+import {rotateArray} from "../../util";
 
+/**
+ * Quantum Random Number Generator (QRNG)
+ */
 export function qrng(): bit {
-  return had(QUBIT_STATE_ZERO).measure();
+    return had(QUBIT_STATE_ZERO).measure();
 }
 
 export function increment(reg: QubitRegister) {
-    const numQubits = reg.numQubits;
-    for(let qubit = 0; qubit < numQubits; qubit++) {
-        const controls = range(qubit + 1, numQubits - 1);
-        mct(reg, controls, qubit);
-    }
+    add(reg, 1);
 }
 
 export function decrement(reg: QubitRegister) {
-    const numQubits = reg.numQubits;
-    for (let qubit = 0; qubit < numQubits; qubit++) {
-        const controls = range(numQubits - qubit, numQubits - 1);
-        const target = numQubits - 1 - qubit;
-        mct(reg, controls, target);
-    }
+    sub(reg, 1);
 }
 
 export function add(reg: QubitRegister, summand: number) {
