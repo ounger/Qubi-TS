0ee476a0e4ebc6a8dbce1a4a0748561a71dac19a
ounger <oliver@oliverunger.de>
2022-12-09 00:16:43 +0100

    Added Test Cases for Bernstein-Vazirani Algorithm

--- src/test/ch.oliverunger/logic/circuits/algorithms/bernstein-vazirani-algorithm.spec.ts
+++ src/test/ch.oliverunger/logic/circuits/algorithms/bernstein-vazirani-algorithm.spec.ts
@@ -10,10 +10,30 @@ import {getNumberAsBitArray} from "../../../../../main/ch.oliverunger/logic/math
 
 describe('Execute Bernstein-Vazirani Algorithm', () => {
 
-    test('A test', () => {
+    function getRandomBitArray(length: number) {
+        const rnd = randomIntFromInterval(0, Math.pow(2, length));
+        return getNumberAsBitArray(rnd, length);
+    }
+
+    test('2 qubits', () => {
         const reg = new QubitRegister(2);
-        const rnd = randomIntFromInterval(0, Math.pow(2, reg.numQubits - 1));
-        const s = getNumberAsBitArray(rnd, reg.numQubits - 1);
+        const s = getRandomBitArray(reg.numQubits - 1);
+        const bvOracle = createBernsteinVaziraniOracle(reg, s);
+        const result = executeBernsteinVaziraniAlgorithm(reg, bvOracle);
+        expect(result).toEqual(s);
+    });
+
+    test('3 qubits', () => {
+        const reg = new QubitRegister(3);
+        const s = getRandomBitArray(reg.numQubits - 1);
+        const bvOracle = createBernsteinVaziraniOracle(reg, s);
+        const result = executeBernsteinVaziraniAlgorithm(reg, bvOracle);
+        expect(result).toEqual(s);
+    });
+
+    test('4 qubits', () => {
+        const reg = new QubitRegister(4);
+        const s = getRandomBitArray(reg.numQubits - 1);
         const bvOracle = createBernsteinVaziraniOracle(reg, s);
         const result = executeBernsteinVaziraniAlgorithm(reg, bvOracle);
         expect(result).toEqual(s);
