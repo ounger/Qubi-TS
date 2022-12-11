a7b5055c22e0525ac5d1a9a4bca7c07ba7aa6fac
oliverunger <oliver@oliverunger.de>
2022-12-11 20:29:39 +0100

    WIP Simons Algorithm

--- src/main/ch.oliverunger/quantum/circuits/algorithms/simons/simons-oracles.ts
+++ src/main/ch.oliverunger/quantum/circuits/algorithms/simons/simons-oracles.ts
@@ -1,15 +1,16 @@
 import {Circuit} from "../../circuit";
 import {QubitRegister} from "../../../multi-qubit/qubit-register";
 import {cx} from "../../../multi-qubit/multi-qubit-gates";
-import {getBitArrayAsNumber, getNumberAsBitArray, xor} from "../../../../util";
+import {getNumberAsBitArray} from "../../../../util";
 import {bit} from "../../../../math/truth-table";
 
 /**
  * Creates a Simon Oracle for a register with n input qubits and n output qubits.
  */
-export function createOneToOneSimonOracle(reg: QubitRegister): Circuit {
+export function createOneToOneSimonsOracle(reg: QubitRegister): Circuit {
     // TODO Check the register
     // TODO Permutation?
+    // TODO Write Tests
     // The hidden bitarray 'secret' is 00...0
     // We copy the content of the first register to the second register
     const circuit = new Circuit();
@@ -20,23 +21,34 @@ export function createOneToOneSimonOracle(reg: QubitRegister): Circuit {
     return circuit;
 }
 
-export function createTwoToOneSimonOracle(reg: QubitRegister, secret: bit[]): Circuit {
+export function createTwoToOneSimonsOracle(reg: QubitRegister, secret: bit[]): Circuit {
     const numInputQubits = reg.numQubits / 2;
     const zeroAsBitArray = getNumberAsBitArray(0, numInputQubits);
     if (zeroAsBitArray === secret) {
         throw new Error("Hidden bitarray 'secret' has to be unequal to the bitarray 00...0, because this would be a one-to-one function!");
     }
-    const checkedInputs = new Array<number>();
-    for (let input = 0; input < Math.pow(2, numInputQubits); input++) {
-        if (checkedInputs.includes(input)) {
-            continue;
-        }
-        const partnerInputAsBitArray: bit[] = xor(secret, getNumberAsBitArray(input, numInputQubits));
-        const partnerInput = getBitArrayAsNumber(partnerInputAsBitArray);
 
-        // TODO
+    const circuit = new Circuit();
 
-        checkedInputs.push(input);
-        checkedInputs.push(partnerInput);
+    // Copy input to output
+    for (let qubit = 0; qubit < numInputQubits; qubit++) {
+        circuit.addGate(() => cx(reg, qubit, numInputQubits + qubit));
     }
+
+    const mostSignificant1BitInSecret = secret.indexOf(1);
+    for (let qubit = 0; qubit < numInputQubits; qubit++) {
+        if (secret[qubit] === 1) {
+            circuit.addGate(() => cx(reg, mostSignificant1BitInSecret, numInputQubits + qubit));
+        }
+    }
+
+    // TODO Permutation
+    //     # Apply a random permutation:
+    //     pos = [
+    //         0,
+    //         len(secret_string) - 1,
+    //     ]  # Swap some qubits to define oracle. We choose first and last:
+    //     yield cirq.SWAP(output_qubits[pos[0]], output_qubits[pos[1]])
+
+    return circuit;
 }
