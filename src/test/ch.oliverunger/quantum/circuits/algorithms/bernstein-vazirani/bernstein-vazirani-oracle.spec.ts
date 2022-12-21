fed986e3371fd3bc1b221c2b8c4155a5b88d6a85
ounger <oliver@oliverunger.de>
2022-12-21 01:29:39 +0100

    Fixed BVO Tests

--- src/test/ch.oliverunger/quantum/circuits/algorithms/bernstein-vazirani/bernstein-vazirani-oracle.spec.ts
+++ src/test/ch.oliverunger/quantum/circuits/algorithms/bernstein-vazirani/bernstein-vazirani-oracle.spec.ts
@@ -7,7 +7,7 @@ import {QUBIT_STATE_ONE, QUBIT_STATE_ZERO} from "../../../../../../main/ch.olive
 
 describe('Create Bernstein-Vazirani Oracle', () => {
 
-    test('Create oracle for 2 qubits in ket(00) and secret 0', () => {
+    test('Create oracle for 2 qubits. Input qubit in ket(0) and secret 0', () => {
         const s: bit[] = [0];
         const reg = QubitRegister.ofQubits(QUBIT_STATE_ZERO, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
@@ -16,16 +16,7 @@ describe('Create Bernstein-Vazirani Oracle', () => {
         expect(output).toEqual(0);
     });
 
-    test('Create oracle for 2 qubits in ket(01) and secret 0', () => {
-        const s: bit[] = [0];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ZERO, QUBIT_STATE_ONE);
-        const oracle = createBernsteinVaziraniOracle(reg, s);
-        oracle.execute();
-        const output = reg.measureSingleQubit(1);
-        expect(output).toEqual(1);
-    });
-
-    test('Create oracle for 2 qubits in ket(10) and secret 0', () => {
+    test('Create oracle for 2 qubits. Input qubit in ket(1) and secret 0', () => {
         const s: bit[] = [0];
         const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
@@ -34,34 +25,16 @@ describe('Create Bernstein-Vazirani Oracle', () => {
         expect(output).toEqual(0);
     });
 
-    test('Create oracle for 2 qubits in ket(11) and secret 0', () => {
-        const s: bit[] = [0];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE);
-        const oracle = createBernsteinVaziraniOracle(reg, s);
-        oracle.execute();
-        const output = reg.measureSingleQubit(1);
-        expect(output).toEqual(1);
-    });
-
-    test('Create oracle for 2 qubits in ket(00) and secret 1', () => {
+    test('Create oracle for 2 qubits. Input qubit in ket(0) and secret 1', () => {
         const s: bit[] = [1];
         const reg = QubitRegister.ofQubits(QUBIT_STATE_ZERO, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
         oracle.execute();
         const output = reg.measureSingleQubit(1);
-        expect(output).toEqual(1);
-    });
-
-    test('Create oracle for 2 qubits in ket(01) and secret 1', () => {
-        const s: bit[] = [1];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ZERO, QUBIT_STATE_ONE);
-        const oracle = createBernsteinVaziraniOracle(reg, s);
-        oracle.execute();
-        const output = reg.measureSingleQubit(1);
         expect(output).toEqual(0);
     });
 
-    test('Create oracle for 2 qubits in ket(10) and secret 1', () => {
+    test('Create oracle for 2 qubits. Input qubit in in ket(1) and secret 1', () => {
         const s: bit[] = [1];
         const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
@@ -70,40 +43,31 @@ describe('Create Bernstein-Vazirani Oracle', () => {
         expect(output).toEqual(1);
     });
 
-    test('Create oracle for 2 qubits in ket(11) and secret 1', () => {
-        const s: bit[] = [1];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE);
+    test('Create oracle for 3 qubits. Input qubits in ket(11) and secret 11', () => {
+        const s: bit[] = [1, 1];
+        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
         oracle.execute();
-        const output = reg.measureSingleQubit(1);
+        const output = reg.measureSingleQubit(2);
         expect(output).toEqual(0);
     });
 
-    test('Create oracle for 3 qubits in ket(111) and secret 11', () => {
+    test('Create oracle for 3 qubits. Input qubits in ket(10) and secret 11', () => {
         const s: bit[] = [1, 1];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE, QUBIT_STATE_ONE);
+        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ZERO, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
         oracle.execute();
-        const output = reg.measureSingleQubit(1);
+        const output = reg.measureSingleQubit(2);
         expect(output).toEqual(1);
     });
 
-    test('Create oracle for 3 qubits in ket(101) and secret 11', () => {
-        const s: bit[] = [1, 1];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ZERO, QUBIT_STATE_ONE);
-        const oracle = createBernsteinVaziraniOracle(reg, s);
-        oracle.execute();
-        const output = reg.measureSingleQubit(1);
-        expect(output).toEqual(0);
-    });
-
-    test('Create oracle for 3 qubits in ket(111) and secret 10', () => {
+    test('Create oracle for 3 qubits. Input qubits in ket(11) and secret 10', () => {
         const s: bit[] = [1, 0];
-        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE, QUBIT_STATE_ONE);
+        const reg = QubitRegister.ofQubits(QUBIT_STATE_ONE, QUBIT_STATE_ONE, QUBIT_STATE_ZERO);
         const oracle = createBernsteinVaziraniOracle(reg, s);
         oracle.execute();
-        const output = reg.measureSingleQubit(1);
-        expect(output).toEqual(0);
+        const output = reg.measureSingleQubit(2);
+        expect(output).toEqual(1);
     });
 
 });
