052cc923cc210d2cc5cea3757b09ffd6a761afd7
ounger <oliver@oliverunger.de>
2023-01-27 19:21:58 +0100

    Test

--- src/test/ch.oliverunger/quantum/algorithms/arithmetic-algorithms.spec.ts
+++ src/test/ch.oliverunger/quantum/algorithms/arithmetic-algorithms.spec.ts
@@ -1,3 +1,15 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Oliver Unger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import {
     executeAddAlgorithm,
     executeSubAlgorithm
@@ -13,6 +25,7 @@ describe("Add two integers", () => {
         }
     });
 
+    // Heavyweight test under some devices
     // test("2096 + 1004 = 3100", () => {
     //     expect(executeAddAlgorithm(4096, 5004)).toEqual(9100);
     // });
@@ -57,6 +70,7 @@ describe("Sub two integers", () => {
         expect(executeSubAlgorithm(11, 11)).toEqual(0);
     });
 
+    // Heavyweight test under some devices
     // test("8044 - 4022 = 4022", () => {
     //     expect(executeSubAlgorithm(8044, 4022)).toEqual(4022);
     // });
